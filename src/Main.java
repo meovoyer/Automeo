e43b3bf9bd752fa781c6c14297b0a680e64cdabc
meo <meo@meo-desktop>
2018-03-19 08:37:16 -0400

    Added the Automeo entry point main function.

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * Automeo's main entry point.
+ * --------------------------
+ * @author meovoyer
+ * Created: 2018-03-19 08:31:29
+ * Updated: 2018-03-19 08:32:12
+ * ----------------------------
+ * This is where all the magic appears!
+ * ------------------------------------
+ */
+public class Main {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/* Create the Automeo GUI */
+		/* Run the program to listen for events */
+	}
+}
